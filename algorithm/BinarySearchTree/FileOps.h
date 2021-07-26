9c4e0f56f3684984a484de961d3cc461ac2fcbf0
Moseasirius <moseasirius.gmail.com>
2021-07-27 01:38:56 +0800

    mo2021/7/27

--- algorithm/BinarySearchTree/FileOps.h
+++ algorithm/BinarySearchTree/FileOps.h
@@ -6,5 +6,21 @@
 #define BINARYSEARCHTREE_FILEOPS_H
 #include <iostream>
 #include <string>
-#
+#include <vector>
+#include <fstream>
+using namespace std;
+// 文件相关操作
+namespace FileOps{
+    int firstCharacterIndex(const string& s,int start){
+        for(int i = start ; i < s.length() ; i++)
+            if(isalpha(s[i]))
+                return i;
+        return s.length();
+    }
+    // 将字符串s中的所有字母转换成小写之后返回
+
+
+
+
+}
 #endif //BINARYSEARCHTREE_FILEOPS_H
