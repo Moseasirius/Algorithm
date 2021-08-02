dc360443f26433964d7bd26f40ecd83db78c72a7
Moseasirius <moseasirius.gmail.com>
2021-08-03 00:23:45 +0800

    mo2021/8/2

---
+++ algorithm/MinimumSpanTrees/cmake-build-debug/CMakeFiles/MinimumSpanTrees.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinimumSpanTrees.dir/main.cpp.obj"
+  "MinimumSpanTrees.exe"
+  "MinimumSpanTrees.exe.manifest"
+  "MinimumSpanTrees.pdb"
+  "libMinimumSpanTrees.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinimumSpanTrees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
