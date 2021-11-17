a445b4cc6b1a8d651d2c76c8f56d3c36baa34bcd
Moseasirius <moseasirius@gmail.com>
2021-11-18 00:05:08 +0800

    FCFS

---
+++ CPlusPlus/cpu_scheduling_algorithms/cmake-build-debug/CMakeFiles/cpu_scheduling_algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu_scheduling_algorithms.dir/main.cpp.obj"
+  "cpu_scheduling_algorithms.exe"
+  "cpu_scheduling_algorithms.exe.manifest"
+  "cpu_scheduling_algorithms.pdb"
+  "libcpu_scheduling_algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpu_scheduling_algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
